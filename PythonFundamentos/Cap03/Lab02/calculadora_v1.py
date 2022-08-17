956b84d5ea749ffe36c0316d4aebdc2285e79f10
Felipe Camilo <70548685+felipecamilo@users.noreply.github.com>
2022-08-17 19:30:09 -0300

    Cap 03 Data analysis - DSA

--- PythonFundamentos/Cap03/Lab02/calculadora_v1.py
+++ PythonFundamentos/Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,42 @@
 
 print("\n******************* Python Calculator *******************")
 
+print('Selecione o número da  operação desejada: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
 
+escolha = input("Digite sua opção (1/2/3/4): ")
+
+def soma():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    soma = num1 + num2
+    print(num1,"+",num2,"=",soma)
+    
+def subtracao():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    subtracao = num1 - num2
+    print(num1,"-",num2,"=",subtracao)
+    
+def multiplicacao():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    multiplicacao = num1 * num2
+    print(num1,"x",num2,"=",multiplicacao)
+    
+def divisao():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    divisao = num1 / num2
+    print(num1,"/",num2,"=",divisao)
+
+
+if escolha == '1':
+    soma()
+elif escolha == '2':
+    subtracao()
+elif escolha == '3':
+    multiplicacao()
+elif escolha == '4':
+    divisao()
+else:
+    print("Escolha inválida!")
